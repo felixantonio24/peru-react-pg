af8ec18d64c7576c83950e46b28b2b762e57dd74
felix <luisfelixtv@outlook.es>
2024-02-06 22:57:36 -0500

    pracs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import './About.css'
+import Tittle from "./tittle";
 
-const About = (props) =>{
+const About = () =>{
 
 
 
@@ -10,21 +11,7 @@ const About = (props) =>{
     <article>
      <div className="container_About"><br />
    <span className='Title_Span'>New news</span>
-
-        <div className="Title-About">
-            <h2 className="Title-Peruvian"> Peruvian coffees receive thirteen awards in the International Competition</h2>
-
-            <div className="img-coffe" >
-               <a href="https://www.gob.pe/institucion/mincetur/noticias/684867-cafes-peruanos-obtienen-trece-premios-en-concurso-internacional" target="_blank" rel="noopener noreferrer"> 
-               <img src="/cafe-img.jpg" alt="coffe"  className="img-cofee" />
-               </a>
-               
-
-            </div>
-
-
-
-        </div>
+<Tittle/>
 
      </div> 
      </article>
---
+++ src/components/Tittle.jsx
@@ -0,0 +1,23 @@
+
+
+const Tittle = () =>{
+
+
+    return(
+        <div className="Title-About">
+        <h2 className="Title-Peruvian"> Peruvian coffees receive thirteen awards in the International Competition</h2>
+
+        <div className="img-coffe" >
+           <a href="https://www.gob.pe/institucion/mincetur/noticias/684867-cafes-peruanos-obtienen-trece-premios-en-concurso-internacional" target="_blank" rel="noopener noreferrer"> 
+           <img src="/cafe-img.jpg" alt="coffe"  className="img-cofee" />
+           </a>
+           
+
+        </div>
+
+
+
+    </div>       
+    )
+}
+export default Tittle
